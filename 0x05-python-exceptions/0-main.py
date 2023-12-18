82d22f7bb236548fc01ec5ae88525af52e862c6f
umuhoza lionel <muhozalionel@gmail.com>
2023-12-18 21:07:06 +0000

    safe prining list

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 my_list = [1, 2, 3, 4, 5]
@@ -10,4 +9,3 @@ nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
-
