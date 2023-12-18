d49270528322c963c401821963674f24becb446e
umuhoza lionel <muhozalionel@gmail.com>
2023-12-18 21:13:14 +0000

    safe prining list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+safe_print_list = __import__('0-safe_print_list').safe_print_list
 
+my_list = [1, 2, 3, 4, 5]
 
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
